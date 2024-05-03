d67de2117cd5297eff8414011d2b6425523463ed
Germanyy0410 <tienduchoang136@gmail.com>
2024-05-03 22:30:50 +0700

    minor refactor

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "IOT ASSIGNMENT"
-python3.8 -m venv venv
-source venv/bin/activate
+python3.8 -m venv env
+source env/bin/activate
 pip install -r requirements.txt
 pip install --upgrade pip
