96a32232ba550eaa1a560e07074624adcee09feb
Germanyy0410 <tienduchoang136@gmail.com>
2024-05-04 10:21:47 +0700

    fix: timer interrupt

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import fsm
 import time
 import random
 import json
+import os
 from controller import *
 # from rs485 import *
 
@@ -12,8 +13,8 @@ from controller import *
 state = {
     "next-cycle": 1,
     "mixer1": 4,
-    "mixer2": 12,
-    "mixer3": 12,
+    "mixer2": 2,
+    "mixer3": 3,
     "pump-in": 2,
     "pump-out": 2,
     "active": 1,
@@ -32,7 +33,7 @@ sched_active = []
 
 def data_callback(feed_id, payload):
     key = feed_id.replace("sys.", "")
-    
+
     if key in state:
 
         # state[key] = payload
@@ -40,10 +41,10 @@ def data_callback(feed_id, payload):
         # control.setActuators(address_feed[key], payload)
         print(f"Updated {key} to {payload}")
         time.sleep(1)
-        
+
     else:
         print(f"No handler found for feed: {feed_id}")
-        
+
 
 # adafruit_client = Adafruit_MQTT()
 client.setRecvCallBack(data_callback)
@@ -51,6 +52,7 @@ client.setRecvCallBack(data_callback)
 
 start_sched = fsm.FarmScheduler()
 control = Physic()
+os.system("clear")
 while True:
 
     if state["active"] == 1:
