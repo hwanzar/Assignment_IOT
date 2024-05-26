d7f561d4de004488f7a610fa650a8767a22e6530
Germanyy0410 <tienduchoang136@gmail.com>
2024-05-26 20:02:38 +0700

    refactor: minor changes

--- fsm.py
+++ fsm.py
@@ -7,7 +7,6 @@ import time
 
 PHYSIC = Physic()
 
-
 class FarmScheduler():
     def __init__(self, debug=True):
         self.debug = debug
@@ -62,7 +61,7 @@ class IdleState(State):
         if self.debug:
             print("IDLE STATE")
         if schedule['next-cycle'] > 0:
-            schedule['next-cycle'] = schedule['next-cycle'] -1
+            schedule['next-cycle'] = schedule['next-cycle'] - 1
             return Mixer1State(debug=self.debug)
         else:
             print("FINISHED !!!")
@@ -70,55 +69,48 @@ class IdleState(State):
 
 class Mixer1State(State):
     def execute(self, schedule):
-        PHYSIC.setActuators(MIXER1,1)
+        PHYSIC.setActuators(MIXER1, 1)
         setTimer(0, int(schedule['mixer1']))
-        # self.wait_for_timer(0)
-        PHYSIC.setActuators(MIXER1,0)
+        PHYSIC.setActuators(MIXER1, 0)
         if self.debug:
             print("MIXER1 STATE - Complete")
         return Mixer2State(debug=self.debug)
 
 class Mixer2State(State):
     def execute(self, schedule):
-        PHYSIC.setActuators(MIXER2,1)
+        PHYSIC.setActuators(MIXER2, 1)
         setTimer(0, int(schedule['mixer2']))
-        # self.wait_for_timer(0)
-        PHYSIC.setActuators(MIXER2,0)
+        PHYSIC.setActuators(MIXER2, 0)
         if self.debug:
             print("MIXER2 STATE - Complete")
         return Mixer3State(debug=self.debug)
 
 class Mixer3State(State):
     def execute(self, schedule):
-        PHYSIC.setActuators(MIXER3,1)
+        PHYSIC.setActuators(MIXER3, 1)
         setTimer(0, int(schedule['mixer3']))
-        # self.wait_for_timer(0)
-        PHYSIC.setActuators(MIXER3,0)
+        PHYSIC.setActuators(MIXER3, 0)
         if self.debug:
             print("MIXER3 STATE - Complete")
         return PumpInState(debug=self.debug)
 
 class PumpInState(State):
     def execute(self, schedule):
-        PHYSIC.setActuators(PUMPIN,1)
+        PHYSIC.setActuators(PUMPIN, 1)
         setTimer(0, int(schedule['pump-in']))
-        # self.wait_for_timer(0)
-        PHYSIC.setActuators(PUMPIN,0)
+        PHYSIC.setActuators(PUMPIN, 0)
         if self.debug:
             print("PUMP IN STATE - Complete")
         return PumpOutState(debug=self.debug)
 
 class PumpOutState(State):
     def execute(self, schedule):
-        PHYSIC.setActuators(PUMPOUT,1)
+        PHYSIC.setActuators(PUMPOUT, 1)
         setTimer(0, int(schedule['pump-out']))
-        # self.wait_for_timer(0)
-        PHYSIC.setActuators(PUMPOUT,0)
+        PHYSIC.setActuators(PUMPOUT, 0)
         if self.debug:
             print("PUMP OUT STATE - Complete")
         return IdleState(debug=self.debug)
-
-
 
 def convert_schedule_json_to_dict(json_data):
     print("DEBUG: We been heerre")
